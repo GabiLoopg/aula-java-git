fb6241c450a5e73f401d203b3b7f29370b49eb30
02956215248 <02956215248@JIPA-LABIN03-31.ifro.local>
2018-03-07 14:58:49 -0400

    Calculadora

--- ProjetoGitJava/src/br/edu/ifro/Aula.java
+++ ProjetoGitJava/src/br/edu/ifro/Aula.java
@@ -14,8 +14,7 @@ public class Aula {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+public static void main(String[] args) {
     System.out.println("Hello World!");
-    }
-    
+}
 }
