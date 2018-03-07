6429614f2fd86ffa6ed391ff7ae855fa5724961c
02956215248 <02956215248@JIPA-LABIN03-31.ifro.local>
2018-03-07 15:12:59 -0400

    Calculadora

--- ProjetoGitJava/src/Calculadora.java
+++ ProjetoGitJava/src/Calculadora.java
@@ -9,20 +9,31 @@
  * @author 02956215248
  */
 public class Calculadora {
-       public double soma (double x, double y){
+       public  static  double soma (double x, double y){
      return x+y;
  }
-    public double subtracao (double x, double y){
+    public static double subtracao (double x, double y){
      return x-y;
  }
-    public double multiplica (double x, double y){
+    public static double multiplica (double x, double y){
      return x*y;
  }
-    public double dividir (double x, double y){
+    public static double dividir (double x, double y){
      return x/y;
  }
-    }
     
 
+public static void main(String[] args){
+    double x =11;
+    double y =20;
+    Calculadora cal=new Calculadora();
+    System.out.println(Calculadora.soma (x, y));
+    System.out.println(Calculadora.subtracao (x, y));
+    System.out.println(Calculadora.multiplica (x, y));
+    System.out.println(Calculadora.dividir (x, y));
+
+}
+    
+}
 
    
