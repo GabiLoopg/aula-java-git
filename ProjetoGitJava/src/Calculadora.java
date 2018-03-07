4dc0b5c847798f79d641c87fa6c35c3cfa55d249
02956215248 <02956215248@JIPA-LABIN03-31.ifro.local>
2018-03-07 14:56:36 -0400

    Calculadora

---
+++ ProjetoGitJava/src/Calculadora.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 02956215248
+ */
+public class Calculadora {
+       public double soma (double x, double y){
+     return x+y;
+ }
+    public double subtracao (double x, double y){
+     return x-y;
+ }
+    public double multiplica (double x, double y){
+     return x*y;
+ }
+    public double dividir (double x, double y){
+     return x/y;
+ }
+    }
+    
+
+
+   
